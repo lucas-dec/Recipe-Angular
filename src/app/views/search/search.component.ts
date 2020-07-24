0783b7f492f6e0546c7289741f505a505fea9d2e
Lucas Dec <dec.lukasz@gmail.com>
2020-07-24 10:52:59 +0200

    fix reload results of search recipes

--- src/app/views/search/search.component.ts
+++ src/app/views/search/search.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChange } from '@angular/core';
 import { RecipesService } from '../../services/recipes.service'
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { SearchRecipes } from '../../models/search-recipes'
 import { SearchResult } from '../../models/search-result'
 
@@ -14,20 +14,22 @@ export class SearchComponent implements OnInit {
   recipes: SearchResult[];
   errorMessage: string;
 
-  constructor(private recipeServices: RecipesService, private route: ActivatedRoute) { }
+  constructor(private recipeServices: RecipesService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    let search
-    this.route.paramMap.subscribe(params => {
-      search = params.get('search')
-    })
-    this.recipeServices.searchRecipes(search).subscribe(recipes => {
-      if (recipes.totalResults === 0) {
-        this.errorMessage = `Sorry, we can't find recipe like: "${search}". Please check and insert name of recipe one more time.`
-      }
-      this.recipes = recipes.results
-    }, error => {
-      this.errorMessage = error
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      if (this.errorMessage) this.errorMessage = ""
+      let search = params.get('search')
+
+      this.recipeServices.searchRecipes(search).subscribe(recipes => {
+        if (recipes.totalResults === 0) {
+          this.errorMessage = `Sorry, we can't find recipe like: "${search}". Please check and insert name of recipe one more time.`
+        }
+        this.recipes = recipes.results
+      }, error => {
+        this.errorMessage = error
+      })
     })
   }
 }
